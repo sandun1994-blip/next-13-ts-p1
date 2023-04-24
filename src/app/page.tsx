83ebe13d14d36c86d0563fe6beaa391658cb2c95
Sandun Tharuka <sandun.suntws@gmail.com>
2023-04-25 02:03:39 +0530

    setup p list

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Paragraph from './components/ui/Paragraph'
+import Heading from './components/ui/LargeHeading'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className="bg-cyan-800">
+     <Heading size='sm' >  yo oy  </Heading>                                             
+    </main>
+  )
+}
